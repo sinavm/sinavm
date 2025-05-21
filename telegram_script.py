9d4f82b3f0be418cb23350e41ef5da6071d82c44
sina <163084137+sinavm@users.noreply.github.com>
2025-05-21 08:56:55 +0330

    Update telegram_script.py

--- telegram_script.py
+++ telegram_script.py
@@ -1,13 +1,13 @@
 import os
 import html
 import json
-from telethon.sync import TelegramClient
+from telethon import TelegramClient
 
 api_id = int(os.getenv("TELEGRAM_API_ID"))
 api_hash = os.getenv("TELEGRAM_API_HASH")
 channel_username = 'sinavm'  # نام کانال شما
 
-client = TelegramClient("anon", api_id, api_hash)
+client = TelegramClient("session.session", api_id, api_hash)  # نام فایل سشن شما
 
 async def main():
     posts_html = '<div class="telegram-posts">\n'
@@ -18,8 +18,9 @@ async def main():
         if message.message and message.message.strip():
             text = html.escape(message.message.strip())
             link = f'https://t.me/{channel_username}/{message.id}'
+            date_str = message.date.strftime('%Y-%m-%d %H:%M')  # فرمت تاریخ خوانا
 
-            posts_html += f'<div class="telegram-post"><a href="{link}" target="_blank" class="post-link">{text}</a><br><small>{message.date}</small></div>\n'
+            posts_html += f'<div class="telegram-post"><a href="{link}" target="_blank" class="post-link">{text}</a><br><small>{date_str}</small></div>\n'
             posts_json.append({
                 "text": message.message.strip(),
                 "date": int(message.date.timestamp()),
